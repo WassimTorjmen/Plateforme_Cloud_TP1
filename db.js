7c801aafa73e71067bcbae5c598b8d86b4423c37
WassimTorjmen <torjmane521@gmail.com>
2024-11-29 18:28:38 +0100

    Second changes on DB

--- db.js
+++ db.js
@@ -2,12 +2,8 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  process.env.DB_NAME, // Nom de la base
-  process.env.DB_USER, // Nom d'utilisateur
-  process.env.DB_PASSWORD, // Mot de passe
+  process.env.DATABSE_URL, // URL BD
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
