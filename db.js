7b6a24c7428930c59648222d86e74bcf23d12054
WassimTorjmen <torjmane521@gmail.com>
2024-11-29 17:47:48 +0100

    Edit DB config

--- db.js
+++ db.js
@@ -1,36 +1,28 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize')
 
-// Configuration de la base de données
+// Database
 const sequelize = new Sequelize(
   process.env.DB_NAME, // Nom de la base
   process.env.DB_USER, // Nom d'utilisateur
   process.env.DB_PASSWORD, // Mot de passe
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
     dialectOptions: {
-      ssl: process.env.DB_SSL === 'true' ? {
+      ssl: {
         require: true,
         rejectUnauthorized: false,
-      } : false,
+      },
     },
     define: {
       createdAt: 'added',
       updatedAt: 'updated',
-    },
-  }
-);
+    }
+  },
+)
 
-// Tester la connexion
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection to the database has been established successfully.');
-    await sequelize.sync();
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-})();
+sequelize.authenticate()
+sequelize.sync()
 
-module.exports = sequelize;
+module.exports = sequelize
